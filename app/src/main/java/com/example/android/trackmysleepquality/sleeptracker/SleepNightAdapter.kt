8ec350ba0e0245cfa6f5e749acce16fa8cbe0e5c
Matija <vorkapicmatija@gmail.com>
2019-10-30 20:08:24 +0100

    Replace ScrollView with RecyclerView and create Adapter for RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -16,9 +16,26 @@
 
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.TextItemViewHolder
+import com.example.android.trackmysleepquality.database.SleepNight
+
 // TODO (02) Create SleepNightAdapter class and extend it
 // from RecyclerView.Adapter<TextItemViewHolder>
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var data = listOf<SleepNight>()
 
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        holder.textView.text = data[position].sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
 // TODO (03) Create a variable, data, that holds a list of SleepNight.
 
 // TODO (04) Override getItemCount() to return the total number of items in the data set.
